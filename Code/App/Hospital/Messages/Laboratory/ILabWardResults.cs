4e16353f787bb8f58a6af3b42029213a0774c929
SzymonZiewiec <SzymonZiewiec@gmail.com>
2015-06-12 14:46:24 +0200

    All webconfigs repaired

---
+++ Code/App/Hospital/Messages/Laboratory/ILabWardResults.cs
@@ -0,0 +1,15 @@
+﻿using NServiceBus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Messages
+{
+    public interface ILabWardResults : ICommand
+    {
+        int PatientID { get; set; }
+        string Comment { get; set; }
+    }
+}
---
+++ Code/App/Hospital/Messages/Ward/Results_PatientRecive.cs
@@ -0,0 +1,14 @@
+﻿using NServiceBus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Messages
+{
+    public class Results_PatientRecive : IResults_PatientRecive
+    {
+        public string Comment { get; set; }
+    }
+}
