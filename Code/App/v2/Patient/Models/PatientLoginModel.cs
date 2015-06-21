f36475ee36b4cc19a8b04b8ab74254e779511767
SzymonZiewiec <SzymonZiewiec@gmail.com>
2015-06-21 18:51:32 +0200

    Adding 2 claass-files for login/egistering @ Patient

---
+++ Code/App/v2/Patient/Models/PatientLoginModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Patient.Models
+{
+    public class PatientLoginModel
+    {
+        [Required(ErrorMessage = "Name is required")]
+        [DisplayName("Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [DisplayName("Password")]
+        public string Password { get; set; }
+    }
+}
