d6676b0679680cbb8d7f150953fb24defd5e8784
SzymonZiewiec <SzymonZiewiec@gmail.com>
2015-06-22 14:53:02 +0200

    New messages for Director / Magazine module

---
+++ Code/App/v2/Messages/Director/IDirectorPatientMessage.cs
@@ -0,0 +1,10 @@
+﻿using NServiceBus;
+
+namespace Messages
+{
+    public interface IDirectorPatientMessage
+    {
+        int MessageId { get; set; }
+        string Content { get; set; }
+    }
+}
