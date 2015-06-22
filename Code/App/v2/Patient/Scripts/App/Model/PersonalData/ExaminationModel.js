b043a09aebf41521c21d236b8e7fdf35ab478e39
SzymonZiewiec <SzymonZiewiec@gmail.com>
2015-06-22 13:53:20 +0200

    Adding new js file

---
+++ Code/App/v2/Patient/Scripts/App/Model/PersonalData/ExaminationModel.js
@@ -0,0 +1,10 @@
+﻿function ExaminationModel(data) {
+    var self = this;
+
+    this.Id = ko.observable(data.Id);
+    this.PatientDieseaseId = ko.observable(data.PatientDieseaseId);
+    this.Comment = ko.observable(data.Comment);
+    this.When = ko.observable(data.When);
+    this.ExaminationType = ko.observable(data.ExaminationType);
+    this.LogType = ko.observable(data.LogType);
+};
